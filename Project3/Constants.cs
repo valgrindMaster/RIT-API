bce74af1aa037decaf18764b6343ecb203ddb21b
Oliver Haynes <drokida@gmail.com>
2017-12-09 00:29:31 -0500

    Finished People and PeopleDetail.

--- Project3/Constants.cs
+++ Project3/Constants.cs
@@ -8,6 +8,6 @@ namespace Project3
 {
     class Constants
     {
-        public const string MAP_STR = "<html><head><style>*{{background-color:#EAECEF;}}iframe{{width:100%;height:100%;overflow:hidden;margin:0 auto;display:block;}}</style></head><body><iframe src=\"{0}\" frameborder=\"0\" scrolling=\"no\"></iframe></body></html>";
+        public const string MAP_STR = "<html><head><style>#map-container{{width:410px;height:610px;position:absolute;top:-10px;right:-10px;bottom:-10px;left:-10px;}}iframe{{overflow:hidden;margin:0 auto;display:block;}}</style></head><body><div id=\"map-container\"><iframe src=\"{0}\" width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\"></iframe></div></body></html>";
     }
 }
--- Project3/Views/PeopleDetail.xaml.cs
+++ Project3/Views/PeopleDetail.xaml.cs
@@ -8,8 +8,20 @@ namespace Project3.Views
 {
     public sealed partial class PeopleDetail : Page
     {
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Tagline { get; set; }
+        public string Office { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Interest { get; set; }
+        public string Twitter { get; set; }
+        public string Facebook { get; set; }
+
         public PeopleDetail()
         {
+            DataContext = this;
             this.InitializeComponent();
         }
 
@@ -18,6 +30,31 @@ namespace Project3.Views
             Person param = (Person)e.Parameter;
             name.Text = param.getName();
             loadProfileImg(param.getImagePath());
+            setProperties(param);
+        }
+
+        private void setProperties(Person person)
+        {
+            Username = setEmptyIfBlank(person.getUsername());
+            Title = setEmptyIfBlank(person.getTitle());
+            Tagline = setEmptyIfBlank(person.getTagline());
+            Office = setEmptyIfBlank(person.getOffice());
+            Email = setEmptyIfBlank(person.getEmail());
+            Phone = setEmptyIfBlank(person.getPhone());
+            Website = setEmptyIfBlank(person.getWebsite());
+            Interest = setEmptyIfBlank(person.getInterestArea());
+            Twitter = setEmptyIfBlank(person.getTwitter());
+            Facebook = setEmptyIfBlank(person.getFacebook());
+        }
+
+        private string setEmptyIfBlank(string val)
+        {
+            if (val == null || val.Length == 0)
+            {
+                return "N/A";
+            }
+
+            return val;
         }
 
         private void loadProfileImg(string imgPath)
